b65b2bb86597fabe0efba231d8ff0becc4b19b11
MHNahib <MHNahib@users.noreply.github.com>
2020-07-16 22:39:13 +0600

    Added a new class named MyApp and cleaned the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Hello World",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello world"),
-        ),
-        body: Material(
-          color: Colors.cyan[600],
-          child: Center(
-            child: Text(
-              "Hello world",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 40.0),
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Hello World",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello world"),
+          ),
+          body: Material(
+            color: Colors.cyan[600],
+            child: Center(
+              child: Text(
+                "Hello world",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white, fontSize: 40.0),
+              ),
             ),
           ),
-        ),
-      )));
+        ));
+  }
 }
